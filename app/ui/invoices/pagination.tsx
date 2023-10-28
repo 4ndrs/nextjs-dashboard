84349449130081a74dcb82acc69efe013629ca61
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-10-29 00:16:56 +0200

    Add search and pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,10 +4,25 @@ import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Link from "next/link";
 import { generatePagination } from "@/app/lib/utils";
+import { usePathname, useSearchParams } from "next/navigation";
 
-export default function Pagination({ totalPages }: { totalPages: number }) {
-  const currentPage = 0;
-  const createPageURL = (a: number) => "";
+type Props = {
+  totalPages: number;
+};
+
+export default function Pagination({ totalPages }: Props) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const currentPage = Number(searchParams.get("page")) || 1;
+
+  const createPageURL = (pageNumber: number) => {
+    const params = new URLSearchParams(searchParams);
+
+    params.set("page", pageNumber.toString());
+
+    return `${pathname}?${params.toString()}`;
+  };
 
   const allPages = generatePagination(currentPage, totalPages);
 
