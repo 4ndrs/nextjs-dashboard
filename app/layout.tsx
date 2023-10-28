786193599327ef32a7642f4d70b76cd523899dfc
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-10-28 10:44:26 +0200

    Add fonts and images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
+import { inter } from "./ui/fonts";
+
 import "./ui/global.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang="en">
+    <body className={`${inter.className} antialiased`}>{children}</body>
+  </html>
+);
+
+export default RootLayout;
